6c433830a969a6b6151edd519307959e69f70fb2
21311433 <rym.kacimi@etu.sorbonne-universite.fr>
2026-03-31 12:11:05 +0200

    J'ai réalisé la partie 2.3.2 et 2.3.3

--- clibeuip.c
+++ clibeuip.c
@@ -1,102 +1,44 @@
- // fichier : clibeuip.c
-
+// fichier : clibeuip.c 
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
-
 #include <strings.h>
-
 #include <sys/types.h>
-
 #include <sys/socket.h>
-
 #include <netinet/in.h>
-
 #include <arpa/inet.h>
-
 #include <netdb.h>
 
-
 int main(int argc, char *argv[]) {
+    int sid;
+    struct sockaddr_in Sock;
+    char buf[512];
 
-int sid;
+    if (argc < 2) {
+        fprintf(stderr, "utilisation : %s [liste | mp pseudo msg | all msg | leave pseudo]\n", argv[0]);
+        return 1;
+    }
 
-struct sockaddr_in Sock;
+    if ((sid = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) return 2;
 
-char buf[512];
+    bzero(&Sock, sizeof(Sock));
+    Sock.sin_family = AF_INET;
+    Sock.sin_port = htons(9998);
+    Sock.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+    bzero(buf, sizeof(buf));
 
-if (argc < 2) {
+    if (strcmp(argv[1], "liste") == 0) {
+        sprintf(buf, "3BEUIP");
+        sendto(sid, buf, 6, 0, (struct sockaddr *)&Sock, sizeof(Sock));
+    } 
+    // code 4: message prive avec insertion manuelle du separateur nul
+    else if (strcmp(argv[1], "mp") == 0 && argc == 4) {
+        sprintf(buf, "4BEUIP%s", argv[2]);
+        int offset = 6 + strlen(argv[2]) + 1;
+        strcpy(buf + offset, argv[3]); // ecriture apres le caractere nul
+        sendto(sid, buf, offset + strlen(argv[3]), 0, (struct sockaddr *)&Sock, sizeof(Sock));
+    }
 
-fprintf(stderr, "utilisation : %s [liste | mp pseudo msg | all msg | leave pseudo]\n", argv[0]);
-
-return 1;
-
+    return 0;
 }
-
-
-if ((sid = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) return 2;
-
-
-bzero(&Sock, sizeof(Sock));
-
-Sock.sin_family = AF_INET;
-
-Sock.sin_port = htons(9998);
-
-Sock.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-
-bzero(buf, sizeof(buf));
-
-
-// code 3: liste
-
-if (strcmp(argv[1], "liste") == 0) {
-
-sprintf(buf, "3BEUIP");
-
-sendto(sid, buf, 6, 0, (struct sockaddr *)&Sock, sizeof(Sock));
-
-}
-
-// code 4: message prive avec separateur nul
-
-else if (strcmp(argv[1], "mp") == 0 && argc == 4) {
-
-sprintf(buf, "4BEUIP%s", argv[2]);
-
-int offset = 6 + strlen(argv[2]) + 1;
-
-strcpy(buf + offset, argv[3]);
-
-sendto(sid, buf, offset + strlen(argv[3]), 0, (struct sockaddr *)&Sock, sizeof(Sock));
-
-}
-
-// code 5: message a tous
-
-else if (strcmp(argv[1], "all") == 0 && argc == 3) {
-
-sprintf(buf, "5BEUIP%s", argv[2]);
-
-sendto(sid, buf, 6 + strlen(argv[2]), 0, (struct sockaddr *)&Sock, sizeof(Sock));
-
-}
-
-// code 0: quitter le reseau
-
-else if (strcmp(argv[1], "leave") == 0 && argc == 3) {
-
-sprintf(buf, "0BEUIP%s", argv[2]);
-
-sendto(sid, buf, 6 + strlen(argv[2]), 0, (struct sockaddr *)&Sock, sizeof(Sock));
-
-}
-
-
-return 0;
-
-} 
--- servbeuip.c
+++ servbeuip.c
@@ -1,4 +1,3 @@
-//servbeuip.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,12 +6,10 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
 
 #define PORT 9998
 #define MAX_CLIENTS 255
 
-// structure pour la table des clients
 struct client {
     struct in_addr ip;
     char pseudo[256];
@@ -25,51 +22,47 @@ int main(int argc, char *argv[]) {
     int sid, n;
     struct sockaddr_in SockConf, Sock, Bcast;
     socklen_t ls;
-    char buf[512];
-    char msg_out[512];
+    char buf[1024];
+    char msg_out[1024];
 
-    // verification du parametre pseudo
     if (argc != 2) {
-        fprintf(stderr, "utilisation : %s pseudo\n", argv[0]);
+        fprintf(stderr, "utilisation : %s pseudo\n", argv[1]);
         return 1;
     }
 
     if ((sid = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) return 2;
 
-    // configuration pour le bind sur le port 9998
+    int opt = 1;
+    setsockopt(sid, SOL_SOCKET, SO_BROADCAST, &opt, sizeof(opt));
+
     bzero(&SockConf, sizeof(SockConf));
     SockConf.sin_family = AF_INET;
     SockConf.sin_port = htons(PORT);
     SockConf.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    if (bind(sid, (struct sockaddr *)&SockConf, sizeof(SockConf)) == -1) return 3;
+    if (bind(sid, (struct sockaddr *)&SockConf, sizeof(SockConf)) < 0) return 3;
 
-    // activation du droit d'envoyer en broadcast
-    int opt = 1;
-    if (setsockopt(sid, SOL_SOCKET, SO_BROADCAST, &opt, sizeof(opt)) < 0) return 4;
-
-    // preparation et envoi du message broadcast initial
     bzero(&Bcast, sizeof(Bcast));
     Bcast.sin_family = AF_INET;
     Bcast.sin_port = htons(PORT);
-    Bcast.sin_addr.s_addr = inet_addr("192.168.88.255");
-    
-    // format du message : code 1 + beuip + pseudo
+    Bcast.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+
     sprintf(msg_out, "1BEUIP%s", argv[1]);
     sendto(sid, msg_out, strlen(msg_out), 0, (struct sockaddr *)&Bcast, sizeof(Bcast));
-    printf("serveur %s demarre et annonce envoyee.\n", argv[1]);
 
     for (;;) {
         ls = sizeof(Sock);
         if ((n = recvfrom(sid, buf, sizeof(buf) - 1, 0, (struct sockaddr *)&Sock, &ls)) == -1) continue;
         buf[n] = '\0';
 
-        // verification de la taille minimale et de la chaine beuip
-        if (n >= 6 && strncmp(buf + 1, "BEUIP", 5) == 0) {
-            char code = buf[0];
-            char *pseudo_recu = buf + 6;
-            
-            // verification des doublons avant ajout a la table
+        if (n < 6 || strncmp(buf + 1, "BEUIP", 5) != 0) continue;
+
+        char code = buf[0];
+        // identification si le message vient de l'hote local
+        int is_local = (Sock.sin_addr.s_addr == inet_addr("127.0.0.1"));
+
+        // codes 1 & 2 : mise a jour de la table (uniquement via reseau)
+        if (!is_local && (code == '1' || code == '2')) {
             int existe = 0;
             for (int i = 0; i < nb_clients; i++) {
                 if (table[i].ip.s_addr == Sock.sin_addr.s_addr) {
@@ -77,20 +70,56 @@ int main(int argc, char *argv[]) {
                     break;
                 }
             }
-            
-            // ajout si nouveau et table non pleine
             if (!existe && nb_clients < MAX_CLIENTS) {
                 table[nb_clients].ip = Sock.sin_addr;
-                strncpy(table[nb_clients].pseudo, pseudo_recu, 255);
+                strncpy(table[nb_clients].pseudo, buf + 6, 255);
                 nb_clients++;
-                printf("nouveau contact ajoute : %s (%s)\n", pseudo_recu, inet_ntoa(Sock.sin_addr));
+                printf("contact ajoute : %s\n", buf + 6);
             }
-
-            // envoi de l'accuse de reception si code 1
             if (code == '1') {
                 sprintf(msg_out, "2BEUIP%s", argv[1]);
-                sendto(sid, msg_out, strlen(msg_out), 0, (struct sockaddr *)&Sock, ls);
+                sendto(sid, msg_out, strlen(msg_out), 0, (struct sockaddr *)&Sock, sizeof(Sock));
             }
+        }
+
+        // code 3 : demande de liste par le client local
+        else if (is_local && code == '3') {
+            printf("--- liste des pairs connectes ---\n");
+            for (int i = 0; i < nb_clients; i++) {
+                printf("[%d] %s -> %s\n", i, table[i].pseudo, inet_ntoa(table[i].ip));
+            }
+        }
+
+        // code 4 : client local demande d'envoyer un message a un pair
+        else if (is_local && code == '4') {
+            char *pseudo_dest = buf + 6;
+            // le message commence juste apres le '\0' du pseudo
+            char *message = pseudo_dest + strlen(pseudo_dest) + 1;
+            
+            int cible = -1;
+            for (int i = 0; i < nb_clients; i++) {
+                if (strcmp(table[i].pseudo, pseudo_dest) == 0) {
+                    cible = i;
+                    break;
+                }
+            }
+
+            if (cible != -1) {
+                sprintf(msg_out, "9BEUIP%s", message);
+                struct sockaddr_in Dest;
+                bzero(&Dest, sizeof(Dest));
+                Dest.sin_family = AF_INET;
+                Dest.sin_port = htons(PORT);
+                Dest.sin_addr = table[cible].ip;
+                sendto(sid, msg_out, 6 + strlen(message), 0, (struct sockaddr *)&Dest, sizeof(Dest));
+            } else {
+                printf("erreur : destinataire %s inconnu\n", pseudo_dest);
+            }
+        }
+
+        // code 9 : reception d'un message venant d'un pair distant
+        else if (!is_local && code == '9') {
+            printf("message de %s : %s\n", inet_ntoa(Sock.sin_addr), buf + 6);
         }
     }
     return 0;
